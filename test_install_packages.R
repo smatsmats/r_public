bd2b21be388f97236de28bab4584ceb69d4518cf
Stephen Mats Mats <willey@washington.edu>
2021-05-29 13:35:46 -0700

    more testing

--- test_install_packages.R
+++ test_install_packages.R
@@ -3,4 +3,7 @@ r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
 
 # for sure these
+install.packages("curl")
+install.packages("xml2")
+install.packages("httr")
 install.packages("aws.s3")
