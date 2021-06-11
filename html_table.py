f20fca1fd319fbd28da8a690bd16649ace3c4f84
John Smith <willey@selby.com>
2021-06-11 14:30:45 -0700

    might as well be pep8y

--- html_table.py
+++ html_table.py
@@ -1,66 +1,61 @@
 #!/usr/bin/python3
-#https://s3-us-west-2.amazonaws.com/rbucket-matsmats/USA_cases_per_hundy.jpg
-#alabama_cases_per_hundy.jpg
-#wa_and_alabama.jpg
-#
-#for 
 
 # no data for 'american_samoa',
-l = ('alabama',
-'alaska',
-'arizona',
-'arkansas',
-'california',
-'colorado',
-'connecticut',
-'delaware',
-'district_of_columbia',
-'florida',
-'georgia',
-'guam',
-'hawaii',
-'idaho',
-'illinois',
-'indiana',
-'iowa',
-'kansas',
-'kentucky',
-'louisiana',
-'maine',
-'maryland',
-'massachusetts',
-'michigan',
-'minnesota',
-'mississippi',
-'missouri',
-'montana',
-'nebraska',
-'nevada',
-'new_hampshire',
-'new_jersey',
-'new_mexico',
-'new_york',
-'north_carolina',
-'north_dakota',
-'northern_mariana_islands',
-'ohio',
-'oklahoma',
-'oregon',
-'pennsylvania',
-'puerto_rico',
-'rhode_island',
-'south_carolina',
-'south_dakota',
-'tennessee',
-'texas',
-'utah',
-'vermont',
-'virgin_islands',
-'virginia',
-'washington',
-'west_virginia',
-'wisconsin',
-'wyoming')
+states = ('alabama',
+          'alaska',
+          'arizona',
+          'arkansas',
+          'california',
+          'colorado',
+          'connecticut',
+          'delaware',
+          'district_of_columbia',
+          'florida',
+          'georgia',
+          'guam',
+          'hawaii',
+          'idaho',
+          'illinois',
+          'indiana',
+          'iowa',
+          'kansas',
+          'kentucky',
+          'louisiana',
+          'maine',
+          'maryland',
+          'massachusetts',
+          'michigan',
+          'minnesota',
+          'mississippi',
+          'missouri',
+          'montana',
+          'nebraska',
+          'nevada',
+          'new_hampshire',
+          'new_jersey',
+          'new_mexico',
+          'new_york',
+          'north_carolina',
+          'north_dakota',
+          'northern_mariana_islands',
+          'ohio',
+          'oklahoma',
+          'oregon',
+          'pennsylvania',
+          'puerto_rico',
+          'rhode_island',
+          'south_carolina',
+          'south_dakota',
+          'tennessee',
+          'texas',
+          'utah',
+          'vermont',
+          'virgin_islands',
+          'virginia',
+          'washington',
+          'west_virginia',
+          'wisconsin',
+          'wyoming')
 
 print("<style>")
 print("  table,")
@@ -72,23 +67,27 @@ print("     border-collapse: collapse;")
 print("  }")
 print("</style>")
 print("<TABLE>")
-for s in l:
+url_base = "https://s3-us-west-2.amazonaws.com/rbucket-matsmats/"
+for s in states:
     t = s.title()
     t = t.replace("_Of_", " of ")
     t = t.replace("_", " ")
     print("  <TR>")
-  
+
     print("    <TD>")
     print("      ", t)
     print("    </TD>")
     print("    <TD>")
-    print("      <A HREF=\"https://s3-us-west-2.amazonaws.com/rbucket-matsmats/%s_cases_per_hundy.jpg\">Cases</A>" % (s))
+    print("      <A HREF=\"%s%s_cases_per_hundy.jpg\">Cases</A>" %
+          (url_base, s))
     print("    </TD>")
     print("    <TD>")
-    print("      <A HREF=\"https://s3-us-west-2.amazonaws.com/rbucket-matsmats/%s_daily_cases.jpg\">Daily Cases</A>" % (s))
+    print("      <A HREF=\"%s%s_daily_cases.jpg\">Daily Cases</A>" %
+          (url_base, s))
     print("    </TD>")
     print("    <TD>")
-    print("      <A HREF=\"https://s3-us-west-2.amazonaws.com/rbucket-matsmats/wa_and_%s.jpg\">Compared to WA</A>" % (s))
+    print("      <A HREF=\"%s%s.jpg\">Compared to WA</A>" %
+          (url_base, s))
     print("    </TD>")
 
     print("  </TR>")
