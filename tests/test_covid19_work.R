5f610148539e03d39d3a95c48a4a1073fbd87f90
Stephen S Mats Mats <willey@washington.edu>
2021-09-08 13:24:54 -0700

    updated tests

--- tests/test_covid19_work.R
+++ tests/test_covid19_work.R
@@ -21,28 +21,27 @@ test_that("init", {
 })
 
 population <- get_population()
-test_that("pop_from_jhu", {
+test_that("pop", {
   expect_true(dim(population)[1] >= 4196)
   expect_true(dim(population)[2] >= 12)
 })
 
 # really don't need all of these and they're likely to break as pops get adjusted
 test_that("get_pop", {
-  expect_equal(get_pop("Maryland", "Baltimore City"), 5.9349, tolerance = 1)
-  expect_equal(get_pop("Washington", "Island"), 0.85141, tolerance = 1)
-  expect_equal(get_pop("Washington", "island"), 0.85141, tolerance = 1)
-  expect_equal(get_pop("Washington"), 76.14893, tolerance = 1)
-  expect_equal(get_pop("Washington", "Total"), 76.14893, tolerance = 1)
-  expect_equal(get_pop("Alabama"), 49.03185, tolerance = 1)
-  expect_equal(get_pop("District Of Columbia"), 7.05749, tolerance = 1)
-  expect_equal(get_pop("District of Columbia"), 7.05749, tolerance = 1)
-  expect_equal(get_pop("district of columbia"), 7.05749, tolerance = 1)
-  expect_equal(get_pop("Puerto Rico"), 31.93694, tolerance = 1)
-  expect_equal(get_pop("american samoa"), 0.55641, tolerance = 1)
+  expect_equal(get_pop(admin1="Maryland", admin2="Baltimore City"), 5.9349, tolerance = 1)
+  expect_equal(get_pop(admin1="Washington", "Island"), 0.85141, tolerance = 1)
+  expect_equal(get_pop(admin1="Washington", "island"), 0.85141, tolerance = 1)
+  expect_equal(get_pop(admin1="Washington"), 76.14893, tolerance = 1)
+  expect_equal(get_pop(admin1="Alabama"), 49.03185, tolerance = 1)
+  expect_equal(get_pop(admin1="District Of Columbia"), 7.05749, tolerance = 1)
+  expect_equal(get_pop(country="US", admin1="District of Columbia"), 7.05749, tolerance = 1)
+  expect_equal(get_pop(admin1="district of columbia"), 7.05749, tolerance = 1)
+  expect_equal(get_pop(admin1="Puerto Rico"), 31.93694, tolerance = 1)
+  expect_equal(get_pop(admin1="american samoa"), 0.55641, tolerance = 1)
   expect_equal(get_pop(country = "india"), 13800.04, tolerance = 1)
   expect_equal(get_pop(country = "Canada"), 378.557, tolerance = 1)
-  expect_equal(get_pop(state = "Washington", county = "Columbia"), 0.03985, tolerance = .01)
-  expect_equal(get_pop("Diamond Princess"), 0)
+  expect_equal(get_pop(admin1 = "Washington", admin2 = "Columbia"), 0.03985, tolerance = .01)
+  expect_equal(get_pop(admin1="Diamond Princess"), 0)
 })
 
 
@@ -55,7 +54,7 @@ test_that("get_pop", {
 # how to test ggplot?
 
 usa_df <- build_all_states()
-test_that("pop_from_jhu", {
+test_that("build usa all", {
   expect_true(dim(usa_df)[1] >= 512)
 })
 
